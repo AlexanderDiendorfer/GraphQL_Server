485610aebe8e4ca26a9235ff36753757c0e6453e
Alexander Diendorfer+ <20122072@htlneufelden.onmicrosoft.comgit config --global user.email 20122072@htlneufelden.onmicrosoft.comgit config --global user.email 20122072@htlneufelden.onmicrosoft.com>
2018-11-14 12:25:06 +0100

    Setting up JSON Server and Axios

--- schema.js
+++ schema.js
@@ -1,11 +1,14 @@
+const axios = require('axios');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList, GraphQLNonNull } = require('graphql');
 
+/*
 // Hardcodes data
 const customers = [
 	{ id: '1', name: 'John Doe', email: 'jdoe@gmail.com', age: 35 },
 	{ id: '2', name: 'Steve Smith', email: 'steve@gmail.com', age: 25 },
 	{ id: '3', name: 'Sara Williams', email: 'sara@gmail.com', age: 32 }
 ];
+*/
 
 // Customer Type
 const CustomerType = new GraphQLObjectType({
@@ -28,17 +31,20 @@ const RootQuery = new GraphQLObjectType({
 				id: { type: GraphQLString }
 			},
 			resolve(parentValue, args) {
+				/*
 				for (let i = 0; i < customers.length; i++) {
 					if (customers[i].id == args.id) {
 						return customers[i];
 					}
-				}
+                }
+                */
+				return axios.get(`http://localhost:3000/customers/${args.id}`).then((res) => res.data);
 			}
 		},
 		customers: {
 			type: new GraphQLList(CustomerType),
 			resolve(parentValue, args) {
-				return customers;
+				return axios.get('http://localhost:3000/customers').then((res) => res.data);
 			}
 		}
 	}
